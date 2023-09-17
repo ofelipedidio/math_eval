780bfe6dbd0602be81b45700dc03463439ceec91
Felipe Didio <felipe.sdidio@gmail.com>
2023-09-17 18:15:55 -0300

    feat: Added parsing to main

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ mod parser;
 
 use anyhow::Result;
 
+use crate::parser::parse_expression;
+
 fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
     let chars: Vec<_> = input.chars().collect();
@@ -13,4 +15,7 @@ fn main() {
     let tokens: Result<Vec<_>> = token_stream.collect();
     let tokens = tokens.unwrap();
     println!("{:?}", &tokens);
+
+    let expression = parse_expression(&tokens).unwrap();
+    println!("{:?}", &expression);
 }
